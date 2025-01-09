50f4661a90d3f8a271bfc05f9fa8243431ec2d89
melissa <msny160@aucklanduni.ac.nz>
2025-01-09 21:17:15 +1300

    Workshop 0 actual

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let x = 0 ;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100, 100, 10);
+  
+  fill(150, 0, 100);
+  rect(mouseX, mouseY, 40, 60)
+  
+  x= x + 3
+
+
+}
